b50bd4d6ce538aee7c92584b03873bde7354e68a
Daniel Popov <dido.popov@gmail.com>
2015-09-24 13:07:00 +0300

    trigger index updating only when the database transaction is commited

--- celery_haystack/utils.py
+++ celery_haystack/utils.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.importlib import import_module
+from django.db import connection as db_connection
 
 from haystack.utils import get_identifier
 
@@ -27,5 +28,11 @@ def enqueue_task(action, instance):
     Common utility for enqueing a task for the given action and
     model instance.
     """
-    identifier = get_identifier(instance)
-    get_update_task().delay(action, identifier)
+
+    # trigger the index updating only when the database transaction has finished!
+    def on_transaction_commit():
+        identifier = get_identifier(instance)
+        update_task = get_update_task()
+        update_task.delay(action, identifier)
+
+    db_connection.on_commit(on_transaction_commit)
