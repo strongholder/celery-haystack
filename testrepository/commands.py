b2e78dd516bf18654500c29d3aa4491283681f7b
Alexander Simeonov <simeonov@gmail.com>
2016-02-15 15:07:52 +0200

    dummy module fix

---
+++ testrepository/commands.py
@@ -0,0 +1,2 @@
+# dummy module to prevent testrepository bug on some servers
+# see https://review.openstack.org/gitweb?p=openstack-dev%2Fpbr.git;a=commitdiff;h=a0cdf7d120a5d2cb24cec4d902db73cad948357c
